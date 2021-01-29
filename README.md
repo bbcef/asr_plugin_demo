0b4ec65dc812946896426815ab2f552346aee4c8
jingbc <961166897@qq.com>
2021-01-29 11:19:16 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# asr_plugin_demo
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+
+集成参考：
++   https://juejin.cn/post/6875600834551447560
++   https://gitee.com/gyy_xiaobaiyang/flutter_baidu_plugin
---
+++ android/app/src/main/kotlin/com/example/asr_plugin_demo/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.asr_plugin_demo
+
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.annotation.NonNull;
+//import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.example.asr_plugin.AsrPlugin
+import io.flutter.app.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+//    fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this));
+        AsrPlugin.registerWith(registrarFor("com.example.asr_plugin.AsrPlugin"));
+    }
+
+//    private void fun registerSelfPlugin() {
+//        AsrPlugin.registerWith(registrarFor("com.example.asr_plugin.AsrPlugin"));
+//    }
+}
+
+
+
+
+
